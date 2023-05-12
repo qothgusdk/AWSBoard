584aaa7357b362e705a34fb34f739188c4656c89
Baesohyun <qothgusdk@naver.com>
2023-05-12 17:16:14 +0900

    게시판 CRUD API 완성

--- src/test/java/com/sh/springboot/web/posts/PostsRepositoryTest.java
+++ src/test/java/com/sh/springboot/web/posts/PostsRepositoryTest.java
@@ -1,4 +1,4 @@
-package com.sh.springboot.posts;
+package com.sh.springboot.web.posts;
 
 import com.sh.springboot.posts.Posts;
 import com.sh.springboot.posts.PostsRepository;
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -42,5 +43,27 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+    @Test
+    public void BaseTimeEntity_등록 () {
+        //given
+        LocalDateTime now = LocalDateTime.of(2023,5,12,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>> createDate="+posts.getCreatedDate()+", modifiedDate="+posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
